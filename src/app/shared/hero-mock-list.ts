7d2810b751c05447349ad5d3418fd73ea62ab67d
Fabricio Di Gennaro <fabriciodigennaro9@gmail.com>
2023-11-20 20:20:13 +0100

    add hero-mock list

---
+++ src/app/shared/hero-mock-list.ts
@@ -0,0 +1,17 @@
+import { Hero } from "../interfaces/hero.interface";
+
+export const HEROES: Hero[] = [
+    { id: 1, name: 'Batman' },
+    { id: 2, name: 'Wonder Woman' },
+    { id: 3, name: 'Hulk' },
+    { id: 4, name: 'Superman' },
+    { id: 5, name: 'Spider-man' },
+    { id: 6, name: 'Black Widow' },
+    { id: 7, name: 'Ant-Man' },
+    { id: 8, name: 'Iron Man' },
+    { id: 9, name: 'Thor' },
+    { id: 10, name: 'Storm' },
+    { id: 11, name: 'Black Canary' },
+    { id: 12, name: 'Daredevil' }
+  ];
+  
