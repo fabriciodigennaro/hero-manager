18914359a1339702e3bf99f20dffa983beeff30b
Fabricio Di Gennaro <fabriciodigennaro9@gmail.com>
2023-11-21 13:11:29 +0100

    improvements in UI

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent {
-
   title: string = 'Hero manager';
-  value: string = '';
+  searchValue = new FormControl('');
 
+  clearSearchForm(): void {
+    this.searchValue.setValue('');
+  }
+
+  searchFormHasValue(): boolean {
+    return this.searchValue.value != undefined && this.searchValue.value != ''; 
+  }
 }
