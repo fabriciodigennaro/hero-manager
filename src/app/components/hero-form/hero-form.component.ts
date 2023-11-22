9a9c64aafaf289048495283907882b16610381d9
Fabricio Di Gennaro <fabriciodigennaro9@gmail.com>
2023-11-22 23:32:20 +0100

    show hero data on edit form

--- src/app/components/hero-form/hero-form.component.ts
+++ src/app/components/hero-form/hero-form.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { Hero } from 'src/app/interfaces/hero.interface';
 import { HeroesService } from 'src/app/services/heroes.service';
 
 @Component({
@@ -8,14 +10,18 @@ import { HeroesService } from 'src/app/services/heroes.service';
   templateUrl: './hero-form.component.html',
   styleUrls: ['./hero-form.component.scss'],
 })
-export class HeroFormComponent {
-  action: string = 'Create';
+export class HeroFormComponent implements OnInit, OnDestroy {
+  private subscriptions: Subscription[] = [];
+  heroId: number = 0;
+  heroToEdit: Hero | null = null;
   form: FormGroup;
+  isEditMode: boolean = false;
 
   constructor(
     private _fb: FormBuilder,
-    private _herosService: HeroesService,
-    private _router: Router
+    private _heroesService: HeroesService,
+    private _router: Router,
+    private route: ActivatedRoute
   ) {
     this.form = this._fb.group({
       name: ['', [Validators.required]],
@@ -23,19 +29,68 @@ export class HeroFormComponent {
     });
   }
 
+  ngOnInit() {
+    const paramsSubscription = this.route.params.subscribe((params) => {
+      this.heroId = params['id'];
+      this.isEditMode = this.heroId != undefined;
+      if (this.isEditMode) {
+        this.loadDataToEdit();
+      }
+    });
+    this.subscriptions.push(paramsSubscription);
+  }
+
+  fetchHeroToEdit(): void {
+    const heroesSubscription = this._heroesService.hero$.subscribe((hero) => {
+      if (hero) {
+        this.heroToEdit = hero;
+        this.setFormValues(hero);
+      }
+    });
+    this.subscriptions.push(heroesSubscription);
+  }
+
+  loadDataToEdit(): void {
+    this.fetchHeroToEdit();
+    this._heroesService.getHeroById(this.heroId);
+  }
+
+  setFormValues(hero: Hero): void {
+    this.form.patchValue(hero);
+  }
+
   isInvalidField(fieldName: string): boolean {
     const field = this.form.get(fieldName);
     return !!field && field.invalid && field.touched;
   }
 
-  onSubmit() {
+  onSubmit(): void {
     if (this.form.valid) {
-      this._herosService.createHero({
-        id: 0,
-        name: this.form.get('name')?.value,
-        description: this.form.get('description')?.value,
-      });
-      this._router.navigate(['/']);
+      console.log(this.heroId);
+
+      if (this.isEditMode) {
+        this._heroesService.updateHero({
+          id: this.heroId,
+          name: this.form.get('name')?.value,
+          description: this.form.get('description')?.value,
+        });
+      } else {
+        this._heroesService.createHero({
+          id: 0,
+          name: this.form.get('name')?.value,
+          description: this.form.get('description')?.value,
+        });
+      }
+
+      this.redirectToDashboard();
     }
+  }
+
+  redirectToDashboard(): void {
+    this._router.navigate(['/']);
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach((subscription) => subscription.unsubscribe());
   }
 }
