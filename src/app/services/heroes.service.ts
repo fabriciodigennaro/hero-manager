eee0015db89a79fa94e01642633642e3ddba049f
Fabricio Di Gennaro <fabriciodigennaro9@gmail.com>
2023-11-26 17:36:18 +0100

    refactor heroes service to use in memory db service and refactor tests

--- src/app/services/heroes.service.ts
+++ src/app/services/heroes.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable, Subject } from 'rxjs';
 
 import { Hero } from '../interfaces/hero.interface';
-import { INITIAL_HEROES_LIST } from '../shared/hero-response-mock-list';
+import { HeroesInMemoryDbService } from './heroes-in-memory-db.service';
 
 @Injectable({ providedIn: 'root' })
 export class HeroesService {
@@ -13,9 +13,8 @@ export class HeroesService {
   private heroesTotalResultsBS: BehaviorSubject<number> = new BehaviorSubject(
     0
   );
-  private heroesDB: Hero[] = INITIAL_HEROES_LIST;
 
-  constructor() {}
+  constructor(private _heroesInMemoryDbService: HeroesInMemoryDbService) {}
 
   get heroesResultsPage$(): Observable<Hero[]> {
     return this.heroesResultsPageBS.asObservable();
@@ -30,17 +29,22 @@ export class HeroesService {
   }
 
   getHeroById(id: number): void {
-    const hero = this.heroesDB.find((hero) => hero.id == id) ?? null;
+    const hero = this._heroesInMemoryDbService.getById(id);
     this.heroBS.next(hero);
   }
 
   searchHeroes(term: string, pageNumber: number, resultsPerPage: number): void {
     const trimmedTerm = term.trim();
+    const allHeroes = this._heroesInMemoryDbService.getAll();
     const foundHeroes = trimmedTerm.length
-      ? this.getFilteredHeroesByName(trimmedTerm)
-      : this.heroesDB;
+      ? this.getFilteredHeroesByName(trimmedTerm, allHeroes)
+      : allHeroes;
     const start = this.calculatePaginatorStart(pageNumber, resultsPerPage);
-    const end = this.calculatePaginatorEnd(pageNumber, resultsPerPage);
+    const end = this.calculatePaginatorEnd(
+      pageNumber,
+      resultsPerPage,
+      foundHeroes.length
+    );
     const paginatedHeroes = foundHeroes.slice(start, end);
 
     this.heroesResultsPageBS.next(paginatedHeroes);
@@ -48,34 +52,26 @@ export class HeroesService {
   }
 
   createHero(hero: Hero): void {
-    const heroId = this.heroesDB.length
-      ? this.heroesDB[this.heroesDB.length - 1].id + 1
-      : 1;
-    hero.id = heroId;
-    this.heroesDB.push(hero);
-    this.heroesResultsPageBS.next(this.heroesDB);
-    this.heroesTotalResultsBS.next(this.heroesDB.length);
+    this._heroesInMemoryDbService.create(hero);
+    const updatedHeroes = this._heroesInMemoryDbService.getAll();
+    this.heroesResultsPageBS.next(updatedHeroes);
+    this.heroesTotalResultsBS.next(updatedHeroes.length);
   }
 
   deleteHero(id: number): void {
-    const heroes = this.heroesDB.filter((hero) => hero.id != id);
-    this.heroesDB = heroes;
-    this.heroesTotalResultsBS.next(this.heroesDB.length);
+    this._heroesInMemoryDbService.delete(id);
+    const updatedHeroes = this._heroesInMemoryDbService.getAll();
+    this.heroesTotalResultsBS.next(updatedHeroes.length);
   }
 
   updateHero(updatedHero: Hero): void {
-    const heroes = this.heroesDB.map((savedHero) => {
-      if (savedHero.id == updatedHero.id) {
-        return updatedHero;
-      }
-      return savedHero;
-    });
-    this.heroesDB = heroes;
-    this.heroesResultsPageBS.next(heroes);
+    this._heroesInMemoryDbService.update(updatedHero);
+    const updatedHeroes = this._heroesInMemoryDbService.getAll();
+    this.heroesResultsPageBS.next(updatedHeroes);
   }
 
-  private getFilteredHeroesByName(term: string): Hero[] {
-    return this.heroesDB.filter((hero) =>
+  private getFilteredHeroesByName(term: string, heroes: Hero[]): Hero[] {
+    return heroes.filter((hero) =>
       hero.name.toLowerCase().includes(term.toLowerCase())
     );
   }
@@ -90,9 +86,10 @@ export class HeroesService {
 
   private calculatePaginatorEnd(
     pageNumber: number,
-    resultsPerPage: number
+    resultsPerPage: number,
+    totalResults: number
   ): number {
     const calculatedEnd = (pageNumber - 1) * resultsPerPage + resultsPerPage;
-    return Math.min(calculatedEnd, this.heroesDB.length);
+    return Math.min(calculatedEnd, totalResults);
   }
 }
